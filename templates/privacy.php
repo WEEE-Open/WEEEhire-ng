fd6d71bfc65c406f681258135e3b86b9a3378ec4
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-10-06 11:56:57 +0200

    Privacy policy++

--- templates/privacy.php
+++ templates/privacy.php
@@ -5,7 +5,7 @@
 <h2>Titolare del trattamento</h2>
 <p>Titolare del trattamento è il Team WEEE Open, associazione non riconosciuta ai sensi degli articoli del 36, 37 e 38 del Codice civile. I contatti del Team WEEE Open sono disponibili sulla pagina "<a href="http://weeeopen.polito.it/contatti.html">contatti</a>" del sito web weeeopen.polito.it oppure all'indirizzo di posta elettronica composto dalla stringa <i>weeeopen</i> seguita dal simbolo @ seguito dal dominio <i>polito.it</i>.</p>
 <h2>Tipi di dati trattati</h2>
-<p>Sono trattati esclusivamente i dati comunicati dall'utente tramite la compilazione e l'invio, esplicito e volontario, dei moduli presenti sul sito.</p>
+<p>Sono trattati i dati comunicati dall'utente tramite la compilazione e l'invio, esplicito e volontario, dei moduli presenti sul sito, in aggiunta all'indirizzo email.</p>
 <p>L'indirizzo email degli utenti viene ottenuto concatenando la matricola, inserita dagli utenti nei moduli, alla stringa "@studenti.polito.it" o "@polito.it" a seconda del tipo di matricola immesso nei moduli.</p>
 <h2>Finalità e base giuridica del trattamento</h2>
 <p>I dati personali verranno trattati al fine di valutare le richieste di candidatura degli utenti. La base giuridica del predetto trattamento è il consenso.</p>
@@ -14,7 +14,8 @@
 <p>Il destinatario dei dati raccolti è il Team WEEE Open.</p>
 <h2>Modalità di trattamento e periodo di conservazione dei dati</h2>
 <p>Il trattamento dei dati sarà svolto in forma manuale da persone autorizzate al trattamento.</p>
-<p>I dati verranno cancellati dal server dopo un tempo massimo di 365 giorni dalla compilazione dei moduli.</p>
+<p>I dati verranno conservati in Paesi membri dell'Unione Europea o in Svizzera, a seconda del server utilizzato al momento. La Svizzera è un Paese che la Commissione Europea ha ritenuto adeguato per il trasferimento di dati personali.</p>
+<p>I dati immessi nel form verranno cancellati da tutti i server dopo un tempo massimo di 365 giorni dalla compilazione dei moduli.</p>
 <p>I dati potranno essere cancellati in qualsiasi momento dal Team WEEE Open qualora non venissero più ritenuti utili al perseguimento delle finalità indicate in precedenza.</p>
 <p>I dati possono essere cancellati in qualsiasi momento dagli utenti tramite la pagina di riepilogo (di seguito la "pagina di riepilogo") che compare dopo l'invio dei moduli e il cui indirizzo viene inoltre inviato via email agli utenti che hanno compilato i moduli.</p>
 <h2>Diritti degli interessati</h2>
