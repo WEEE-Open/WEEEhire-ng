fdbf7a7f9ab17d5dc32f510e3b9c0003367f2c0e
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-10-08 11:33:11 +0200

    Fix navbar

--- templates/navbar.php
+++ templates/navbar.php
@@ -2,14 +2,11 @@
 <?php /* @var $authenticated bool */ ?>
 <nav class="navbar navbar-expand-sm top">
 	<div class="container">
-	    <a class="navbar-brand mr-auto" href="/">
-		    <img src="weee.png" height="69" class="img-responsive" alt="WEEE Open">
+	    <a class="navbar-brand mr-3" href="/">
+		    <img src="weee.png" height="69" class="img-fluid" alt="WEEE Open">
 	    </a>
-		<div>
+		<div class="ml-auto">
 			<a href="language.php?l=en-us&from=<?= rawurlencode($_SERVER['REQUEST_URI']) ?>">en&nbsp;🇺🇸</a>&nbsp;-&nbsp;<a href="language.php?l=it-it&from=<?= rawurlencode($_SERVER['REQUEST_URI']) ?>">it&nbsp;🇮🇹</a>
 		</div>
-	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="navbar-toggler-icon"></span>
-	    </button>
 	</div>
 </nav>
