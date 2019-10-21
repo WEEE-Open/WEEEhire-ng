888d7c9e964246639322d19dae817c438de9f776
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-10-21 23:49:43 +0200

    Buttons

--- templates/settings.php
+++ templates/settings.php
@@ -19,24 +19,28 @@ $this->layout('base', ['title' => __('Opzioni WEEEHire'), 'datatables' => true])
 	<p><i><?=__('Modifica scadenza candidature e altri parametri di configurazione WEEEHire')?></i></p>
 	<form method="post">
 		<div class="form-group row">
-			<div class="col-md-9">
+			<div class="<?= $expiry === null ? 'col-sm-12 col-lg-8' : 'col-sm-12 col-lg-6' ?>">
 				<label for="expiry">
 					<?=sprintf(__('Scadenza Candidature <b>(%s)</b>'),
 						$expiry === null ? __('Nessuna Scadenza') : $expiry->format('d-m-Y'))?>
 				</label>
 			</div>
-			<div class="col-md-2">
+			<div class="col-sm-6 col-lg-3">
 				<input type="date" class="form-control" name="expiry" id="expiry"
 						value="<?=$expiry === null ? '' : $expiry->format('Y-m-d');?>">
 			</div>
-			<div class="col-md-1">
-				<?php if($expiry === null): ?>
+			<?php if($expiry === null): ?>
+				<div class="col-sm-6 col-lg-1 mt-3 mt-sm-0">
 					<button type="submit" class="btn btn-primary"><?=__('Conferma')?></button>
-				<?php else: ?>
+				</div>
+			<?php else: ?>
+				<div class="col-sm-6 col-lg-3 mt-3 mt-sm-0">
+					<button type="submit" class="btn btn-primary"><?=__('Conferma')?></button>
 					<button type="submit" class="btn btn-outline-danger" name="noexpiry"
 							value="true">&#x274C;<?=__('Elimina')?></button>
-				<?php endif ?>
-			</div>
+				</div>
+			<?php endif ?>
 		</div>
-	</form>
+</div>
+</form>
 </div>
