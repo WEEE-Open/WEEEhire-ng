92a4d2dc49a4a58452e8f32c3383c4019e6ca7bc
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-10-22 22:49:42 +0200

    Add buttons, close #8

--- templates/index.php
+++ templates/index.php
@@ -6,14 +6,12 @@ $this->layout('base', ['title' => 'WEEElcome']) ?>
 <h1><?=__('Entra in WEEE Open')?></h1>
 <p><?=__('Compila il questionario per fare richiesta di ammissione in team. Premi il bottone qui sotto per iniziare.')?></p>
 <div class="col-md-12 text-center">
-	<a class="btn btn-lg btn-primary the-button" href="form.php"><?=__('Inizia')?></a>
+	<?php if($_SESSION['locale'] === 'en-us'): ?>
+		<a class="btn btn-lg btn-primary the-button" href="form.php">Begin (in English)</a><a class="btn btn-lg btn-outline-secondary the-button" href="language.php?l=it-it&from=<?=rawurlencode('/form.php')?>">Inizia (in italiano)</a>
+	<?php else: ?>
+		<a class="btn btn-lg btn-primary the-button" href="form.php">Inizia (in italiano)</a><a class="btn btn-lg btn-outline-secondary the-button" href="language.php?l=en-us&from=<?=rawurlencode('/form.php')?>">Begin (in English)</a>
+	<?php endif; ?>
 </div>
-<?php if($_SESSION['locale'] === 'en-us'): ?>
-	<p>Il questionario è anche disponibile <a
-				href="language.php?l=it-it&from=<?=rawurlencode('/form.php')?>">in Italiano</a></p>
-<?php else: ?>
-	<p>The form is also available <a href="language.php?l=en-us&from=<?=rawurlencode('/form.php')?>">in English</a></p>
-<?php endif ?>
 <?php if($expiry !== null): ?>
 	<p id="timer"></p>
 	<script>
