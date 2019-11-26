47984d43637d5f5dde89b81f5d4966560989f86e
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-11-26 22:15:33 +0100

    Reformat

--- templates/notification_email.php
+++ templates/notification_email.php
@@ -3,15 +3,22 @@
 /** @var $user \WEEEOpen\WEEEHire\User */
 
 $saluti1 = ['Ciao persone,', 'Welà gente,', 'Hey persone,', 'Ciao,', 'Buongiornissimo,', 'Hi,'];
-$saluti2 = ['- Il WEEEHire', "Cordiali saluti,\nWEEEHire", 'Statemi bene', 'Ciaone', "Cordialmente vostro,\nWEEEHire", 'Allego informali convenevoli [sic]'];
+$saluti2 = [
+	'- Il WEEEHire',
+	"Cordiali saluti,\nWEEEHire",
+	'Statemi bene',
+	'Ciaone',
+	"Cordialmente vostro,\nWEEEHire",
+	'Allego informali convenevoli [sic]'
+];
 
 $rand1 = mt_rand(0, count($saluti1) - 1);
 $rand2 = mt_rand(0, count($saluti2) - 1);
 ?>
 
-<?= $saluti1[$rand1] ?>
+<?=$saluti1[$rand1]?>
 
-c'è una nuova candidatura per l'area <?= $user->area ?>.
-Andate a valutarla: <?= $link ?>
+c'è una nuova candidatura per l'area <?=$user->area?>.
+Andate a valutarla: <?=$link?>
 
-<?= $saluti2[$rand2] ?>
+<?=$saluti2[$rand2]?>
