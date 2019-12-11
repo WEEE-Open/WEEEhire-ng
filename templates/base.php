7482d7c2566d0383a61068851cdb8b57502e0f59
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-12-11 12:02:53 +0100

    Translate untranslatable string

--- templates/base.php
+++ templates/base.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title><?=$this->e($title)?> - Join WEEE Open</title>
+	<title><?=$this->e($title)?> - <?=__('Entra in WEEE Open')?></title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 			integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="weee.css">
