6ccb7a62894d21c66ab14d8cfacf25f5bb1f1363
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-12-15 13:39:19 +0100

    More and more tests
    A bit too tangled maybe, but how can we even split them?

--- tests/PageCandidatesAndPageStatusTest.php
+++ tests/PageCandidatesAndPageStatusTest.php
@@ -52,10 +52,14 @@ class PageCandidatesAndPageStatusTest extends PagesTest {
 		// Candidates page
 		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/candidates.php'], [], [], [], []);
 		$response = (new PageCandidates())->handle($request);
-
 		$this->assertEquals(200, $response->getStatusCode(), '200 OK');
 		$this->assertStringContainsString('Test Test', $response->getBody());
 		$this->assertStringContainsStringIgnoringCase('1 da valutare', $response->getBody());
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+
+		return [$id, $token];
 	}
 
 	public function testNewTwice() {
@@ -72,19 +76,7 @@ class PageCandidatesAndPageStatusTest extends PagesTest {
 	}
 
 	public function testApproved() {
-		list($id, $token) = $this->addCandidate();
-
-		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/candidates.php'], ['id' => $id], [], [], []);
-		$response = (new PageCandidates())->handle($request);
-
-		$this->assertEquals(200, $response->getStatusCode(), '200 OK');
-		$this->assertStringContainsString('Test', $response->getBody());
-		$this->assertStringContainsStringIgnoringCase('approva', $response->getBody());
-		$this->assertStringContainsStringIgnoringCase('rifiuta', $response->getBody());
-		$this->assertStringContainsStringIgnoringCase('note', $response->getBody());
-		if(session_status() === PHP_SESSION_ACTIVE) {
-			session_write_close();
-		}
+		list($id, $token) = $this->testNew();
 
 		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'POST', 'REQUEST_URI' => '/candidates.php'], ['id' => $id], ['approve' => 'true', 'notes' => 'notes example os8mieh7saich4rohZ6E'], [], []);
 		$response = (new PageCandidates())->handle($request);
@@ -107,14 +99,156 @@ class PageCandidatesAndPageStatusTest extends PagesTest {
 		}
 
 		// Status page
-		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => "/candidates.php"], ['id' => $id, 'token' => $token], [], [], []);
+		$this->assertStatusPageWait($id, $token);
+		return [$id, $token];
+	}
+
+	public function testApprovedRevert() {
+		list($id, $token) = $this->testApproved();
+
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'POST', 'REQUEST_URI' => '/candidates.php'], ['id' => $id], ['limbo' => 'true', 'notes' => 'notes example os8mieh7saich4rohZ6E'], [], []);
+		$response = (new PageCandidates())->handle($request);
+		$this->assertEquals(303, $response->getStatusCode(), 'Redirect to same page');
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/candidates.php'], ['id' => $id], [], [], []);
+		$response = (new PageCandidates())->handle($request);
+
+		$this->assertEquals(200, $response->getStatusCode(), '200 OK');
+		$this->assertStringContainsString('Test', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('approva', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('rifiuta', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('note', $response->getBody());
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+
+		// Status page
+		$this->assertStatusPageWait($id, $token);
+	}
+
+	public function testRejected() {
+		list($id, $token) = $this->addCandidate();
+
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'POST', 'REQUEST_URI' => '/candidates.php'], ['id' => $id], ['reject' => 'true', 'notes' => 'notes example Aew9mahdohjee6chaese'], [], []);
+		$response = (new PageCandidates())->handle($request);
+		$this->assertEquals(303, $response->getStatusCode(), 'Redirect to same page');
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/candidates.php'], ['id' => $id], [], [], []);
+		$response = (new PageCandidates())->handle($request);
+		$this->assertEquals(200, $response->getStatusCode(), '200 OK');
+		$this->assertStringContainsString('Test', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('rimanda nel limbo', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('pubblica', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('note', $response->getBody());
+		$this->assertStringContainsString('notes example Aew9mahdohjee6chaese', $response->getBody());
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+
+		$this->assertStatusPageWait($id, $token);
+
+		// Now publish it!
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'POST', 'REQUEST_URI' => '/candidates.php'], ['id' => $id], ['publishnow' => 'true', 'notes' => 'notes example uroh7reith8aet6Yoish'], [], []);
+		$response = (new PageCandidates())->handle($request);
+		$this->assertEquals(303, $response->getStatusCode(), 'Redirect to same page');
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/candidates.php'], ['id' => $id], [], [], []);
+		$response = (new PageCandidates())->handle($request);
+		$this->assertStringContainsString('Test', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('rimanda nel limbo', $response->getBody());
+		$this->assertStringContainsString('Risultati pubblicati', $response->getBody());
+		$this->assertStringContainsString('danger', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('note', $response->getBody());
+		$this->assertStringContainsString('notes example uroh7reith8aet6Yoish', $response->getBody());
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+
+		$this->assertStatusPageRejected($id, $token);
+
+		return [$id, $token];
+	}
+
+	private function assertStatusPageWait($id, $token): void {
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => "/candidates.php"],
+			['id' => $id, 'token' => $token], [], [], []);
 		$response = (new PageStatus())->handle($request);
 		$this->assertEquals(200, $response->getStatusCode(), '200 OK');
 		$this->assertStringContainsStringIgnoringCase('in corso', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('ammesso/a', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('respinta', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('rimandat', $response->getBody());
+
 		$this->assertStringContainsString('status-waiting', $response->getBody());
 		$this->assertStringNotContainsString('status-approved', $response->getBody());
 		$this->assertStringNotContainsString('status-rejected', $response->getBody());
 		$this->assertStringNotContainsString('status-postponed', $response->getBody());
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+	}
+
+	private function assertStatusPageApproved($id, $token): void {
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => "/candidates.php"],
+			['id' => $id, 'token' => $token], [], [], []);
+		$response = (new PageStatus())->handle($request);
+		$this->assertEquals(200, $response->getStatusCode(), '200 OK');
+		$this->assertStringNotContainsStringIgnoringCase('in corso', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('ammesso/a', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('respinta', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('rimandat', $response->getBody());
+
+		$this->assertStringNotContainsString('status-waiting', $response->getBody());
+		$this->assertStringContainsString('status-approved', $response->getBody());
+		$this->assertStringNotContainsString('status-rejected', $response->getBody());
+		$this->assertStringNotContainsString('status-postponed', $response->getBody());
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+	}
+
+	private function assertStatusPageRejected($id, $token): void {
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => "/candidates.php"],
+			['id' => $id, 'token' => $token], [], [], []);
+		$response = (new PageStatus())->handle($request);
+		$this->assertEquals(200, $response->getStatusCode(), '200 OK');
+		$this->assertStringNotContainsStringIgnoringCase('in corso', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('ammesso/a', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('respinta', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('rimandat', $response->getBody());
+
+		$this->assertStringNotContainsString('status-waiting', $response->getBody());
+		$this->assertStringNotContainsString('status-approved', $response->getBody());
+		$this->assertStringContainsString('status-rejected', $response->getBody());
+		$this->assertStringNotContainsString('status-postponed', $response->getBody());
+		if(session_status() === PHP_SESSION_ACTIVE) {
+			session_write_close();
+		}
+	}
+
+	private function assertStatusPageHold($id, $token): void {
+		$request = ServerRequestFactory::fromGlobals(['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => "/candidates.php"],
+			['id' => $id, 'token' => $token], [], [], []);
+		$response = (new PageStatus())->handle($request);
+		$this->assertEquals(200, $response->getStatusCode(), '200 OK');
+		$this->assertStringNotContainsStringIgnoringCase('in corso', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('ammesso/a', $response->getBody());
+		$this->assertStringNotContainsStringIgnoringCase('respinta', $response->getBody());
+		$this->assertStringContainsStringIgnoringCase('rimandat', $response->getBody());
+
+		$this->assertStringNotContainsString('status-waiting', $response->getBody());
+		$this->assertStringNotContainsString('status-approved', $response->getBody());
+		$this->assertStringNotContainsString('status-rejected', $response->getBody());
+		$this->assertStringContainsString('status-postponed', $response->getBody());
 		if(session_status() === PHP_SESSION_ACTIVE) {
 			session_write_close();
 		}
