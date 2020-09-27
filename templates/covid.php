43b68fc0eb1e1018f7a9de21fc0c998ae9f4c4cd
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2020-09-27 11:11:39 +0200

    COVID updated

--- templates/covid.php
+++ templates/covid.php
@@ -1,5 +1,5 @@
 <div class="alert alert-warning coronavirus">
-	<p><?=__('A causa dell\'emergenza coronavirus COVID-19, tutte le attività del team all\'interno del Politecnico sono sospese fino alla data in cui riprenderanno regolarmente le lezioni.')?></p>
-	<p><?=__('Puoi comunque candidarti per alcune posizioni: sosterrai l\'eventuale colloquio da remoto, oppure appena le attività riprenderanno.')?></p>
-	<p><?=__('Per i ruoli che prevedono la <b>possibilità di lavorare da remoto (sysadmin, designer, programmatori)</b> potrai comunque svolgere attività da casa.')?></p>
+	<p><?=__('Da Ottobre 2020 <strong>le attività del team sono riprese</strong>, anche in presenza, con delle limitazioni sul numero massimo di persone in laboratorio.')?></p>
+	<p><?=__('Pertanto non sappiamo ancora se riusciremo ad aprire le candidature per il ruolo <strong>riparazione hardware</strong>: se ti interessa quello, torna a controllare tra qualche giorno. Questo messaggio verrà aggiornato in caso di cambiamenti.')?></p>
+	<p><?=__('Tutti gli altri ruoli invece consentono di svolgere attività da casa, del tutto o in parte.')?></p>
 </div>
