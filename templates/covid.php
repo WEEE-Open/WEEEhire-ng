4a3fda1d0d6334bc7a8ecded7d0ae1468bfc436f
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2020-10-02 00:14:28 +0200

    Reopen hardware repairs

--- templates/covid.php
+++ templates/covid.php
@@ -1,5 +1,4 @@
 <div class="alert alert-warning coronavirus">
-	<p><?=__('Da Ottobre 2020 <strong>le attività del team sono riprese</strong>, anche in presenza, con delle limitazioni sul numero massimo di persone in laboratorio.')?></p>
-	<p><?=__('Pertanto non sappiamo ancora se riusciremo ad aprire le candidature per il ruolo <strong>riparazione hardware</strong>: se ti interessa quello, torna a controllare tra qualche giorno. Questo messaggio verrà aggiornato in caso di cambiamenti.')?></p>
+	<p><?=__('Da Ottobre 2020 <strong>le attività del team sono riprese</strong>, anche in presenza, con delle limitazioni sul numero massimo di persone in laboratorio. È pertanto possibile candidarsi anche nella categoria <strong>riparazione hardware</strong>.')?></p>
 	<p><?=__('Tutti gli altri ruoli invece consentono di svolgere attività da casa, del tutto o in parte.')?></p>
 </div>
