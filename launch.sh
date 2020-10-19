1297c18ca9d4d5a7ef3ab1a8b72c763b45ac7823
Luca Errani <luca.errani1@gmail.com>
2020-10-19 18:05:18 +0200

    Fix launch scritp

--- launch.sh
+++ launch.sh
@@ -1,18 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ ! type sqlite3 > /dev/null ]]; then
+if ! type sqlite3 > /dev/null ; then
 	echo "You need to install sqlite3 first, exiting"
 	exit 1
 fi
-if [[ ! type docker > /dev/null ]]; then
+if ! type docker > /dev/null ; then
 	echo "You need to install docker first, exiting"
 	exit 1
 fi
-if [[ ! type docker-compose > /dev/null ]]; then
+if ! type docker-compose > /dev/null ; then
 	echo "You need to install docker-compose first, exiting"
 	exit 1
 fi
 echo "Creating database file"
-sqlite3 weeehire.db < database.sql
-
+if [[ ! -f weeehire.db ]]; then
+	sqlite3 weeehire.db < database.sql
+fi
 docker-compose up
