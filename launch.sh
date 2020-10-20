73009320d047fb5634de3aea3fb344a3b75891ca
Luca Errani <luca.errani1@gmail.com>
2020-10-20 10:37:05 +0200

    launch.sh script refactor

--- launch.sh
+++ launch.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+function check_or_exit {
+	if ! type $1 > /dev/null ; then
+		echo "You need to install $1 first, exiting"
+		exit 1
+	fi
+}
 
-if ! type sqlite3 > /dev/null ; then
-	echo "You need to install sqlite3 first, exiting"
-	exit 1
-fi
-if ! type docker > /dev/null ; then
-	echo "You need to install docker first, exiting"
-	exit 1
-fi
-if ! type docker-compose > /dev/null ; then
-	echo "You need to install docker-compose first, exiting"
-	exit 1
-fi
+check_or_exit sqlite3
+check_or_exit docker
+check_or_exit docker-compose
+
 echo "Creating database file"
 if [[ ! -f weeehire.db ]]; then
 	sqlite3 weeehire.db < database.sql
