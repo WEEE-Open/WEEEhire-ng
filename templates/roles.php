1e4ab38c2501ea65e721780ffe952b8d8a6ebd0b
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2021-03-06 22:25:17 +0100

    Add the thing where it should go

--- templates/roles.php
+++ templates/roles.php
@@ -5,6 +5,7 @@ function getRoles() {
 		'Elettronica'                     => __('Elettronica'),
 		'Sviluppo software Python'        => __('Sviluppo software Python'),
 		'Sviluppo software PHP'           => __('Sviluppo software PHP'),
+		'Sviluppo software JavaScript'    => __('Sviluppo software JavaScript'),
 		'Sysadmin'                        => __('Sysadmin'),
 		'Creazione di contenuti digitali' => __('Creazione di contenuti digitali'),
 		'Riuso creativo'                  => __('Riuso creativo'),
