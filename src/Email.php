7c95ef4c33bbb89877471da90fac980fe5b1c6ae
Alessandro Celoria <alessandro.celoria@hotmail.com>
2023-02-27 03:46:18 +0100

    Fix emails

--- src/Email.php
+++ src/Email.php
@@ -19,32 +19,33 @@ class Email
 	{
 		if (defined('TEST_MODE') && TEST_MODE) {
 			error_log("Test mode enabled, NOT sending email to $to, subject: $subject\nBody:\n$body");
-
 			return;
 		}
 
 		$mail = new PHPMailer(true);
-		$mail->isMail();
+		$mail->isSMTP();
 		$mail->CharSet = 'utf-8';
 
-		try {
-			$mail->setFrom('noreply@join.weeeopen.it', 'WEEE Open');
-			$mail->addAddress($to);
+		$mail->setFrom('noreply@join.weeeopen.it', 'WEEE Open');
+		$mail->addAddress($to);
 
-			// Quirks mode! That mail server converts 8bit to quoted-printable, which is not a problem.
-			// Unless it has to forward the email to another address, then the DKIM validation fails and nothing is
-			// forwarded. Let's hope that this prevents the mail server from needlessly tampering with the content.
-			if (Utils::endsWith(strtolower($to), '@polito.it')) {
-				$mail->Encoding = PHPMailer::ENCODING_QUOTED_PRINTABLE;
-			}
-
-			$mail->isHTML(false);
-			$mail->Subject = $subject;
-			$mail->Body = $body;
-
-			$mail->send();
-		} catch (Exception $e) {
-			throw new MailException();
+		// Quirks mode! That mail server converts 8bit to quoted-printable, which is not a problem.
+		// Unless it has to forward the email to another address, then the DKIM validation fails and nothing is
+		// forwarded. Let's hope that this prevents the mail server from needlessly tampering with the content.
+		if (Utils::endsWith(strtolower($to), '@polito.it')) {
+			$mail->Encoding = PHPMailer::ENCODING_QUOTED_PRINTABLE;
 		}
+
+		$mail->isHTML(false);
+		$mail->Subject = $subject;
+		$mail->Body = $body;
+		
+		// Talk to the SMTP server in a sibling container over the podman network.
+		// Do not use any TLS or Auth as the entire process is internal.
+		$mail->Host = 'mail';
+		$mail->SMTPSecure = false;
+		$mail->SMTPAutoTLS = false;
+
+		$mail->send();
 	}
 }
