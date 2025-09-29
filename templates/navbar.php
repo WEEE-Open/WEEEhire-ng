937beba55f3da4994994c1bf3a5e32dabc73646c
Leone25 <franciscono.enry@gmail.com>
2025-09-29 18:23:09 +0200

    add query to language change url

--- templates/navbar.php
+++ templates/navbar.php
@@ -12,6 +12,13 @@
 /**
  * @var \Psr\Http\Message\UriInterface $globalRequestUri
  */
+
+$return_url = $globalRequestUri->getPath();
+$query = $globalRequestUri->getQuery();
+if ($query != '') {
+	$return_url .= '?' . $query;
+}
+$return_url = rawurlencode($return_url);
 ?>
 <nav class="navbar navbar-expand-sm top">
 	<div class="container">
@@ -19,8 +26,8 @@
 			<img src="weee.png" height="69" class="img-fluid" alt="WEEE Open">
 		</a>
 		<div class="ml-auto">
-			<a href="language.php?l=en-US&amp;from=<?php echo rawurlencode($globalRequestUri->getPath())?>">en</a>&nbsp;-&nbsp;<a
-					href="language.php?l=it-IT&amp;from=<?php echo rawurlencode($globalRequestUri->getPath())?>">it</a>
+			<a href="language.php?l=en-US&amp;from=<?=$return_url?>">en</a>&nbsp;-&nbsp;<a
+					href="language.php?l=it-IT&amp;from=<?=$return_url?>">it</a>
 		</div>
 	</div>
 </nav>
